fb06cf7d20c81605d6259e4bf087f1567b0898ad
JACKYCHU5200321 <chuwaichun20031230@gmail.com>
2025-06-10 15:08:25 +0800

    second version

--- project/wesite/sensordata/urls.py
+++ project/wesite/sensordata/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('main/', views.page_timeevent),
     path('list/', views.page_listrecords),
+    path('list/json/', views.recordasjson),
     path('query/', views.page_query),
     path('homepage/', views.home),
     path('<str:any>/', views.page_not_found),
